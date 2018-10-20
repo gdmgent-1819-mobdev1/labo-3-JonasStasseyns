9682e3c1583f9fb102127e5ad6df5534b9b9651e
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2018-10-20 13:45:44 +0200

    JSON.Parse Error
    geolocation fetched
    returns object which contains longitude and latitude
    stringifying and saving to localstorage
    JSON.parsing from localstorage but returns empty object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+(function () {
+    //console.log(localStorage.getItem('clientCoords'));
+})();
+
+
+
 function fetchData() {
     //HTML5 Fetch
     fetch('https://randomuser.me/api?results=10')
@@ -7,7 +13,25 @@ function fetchData() {
         .then(function (myJson) {
             localStorage.setItem('fetched-profiles', JSON.stringify(myJson));
             validateFetch();
+            //navigator.geolocation.getCurrentPosition(locationSuccess);
         })
+
+
+
+}
+
+if ("geolocation" in navigator) {
+    if (localStorage.getItem('coords') == undefined) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            let coord = [];
+            coord['longitude'] = pos.coords.longitude;
+            coord['latitude'] = pos.coords.latitude;
+            console.table(coord);
+            localStorage.setItem('coords', JSON.stringify(coord));
+        });
+    }
+} else {
+    console.log("Geolocation request denied or not available");
 }
 
 function validateFetch() {
@@ -22,6 +46,7 @@ function validateFetch() {
             console.log('duplicate found');
             localStorage.removeItem('fetched-profiles');
             fetchData();
+
         }
     }
     if (clear) {
@@ -30,11 +55,11 @@ function validateFetch() {
     }
 }
 
-document.querySelector('.like').addEventListener('click', function(){
+document.querySelector('.like').addEventListener('click', function () {
     ClassifyProfile('like');
-    
+
 });
-document.querySelector('.dislike').addEventListener('click', function(){
+document.querySelector('.dislike').addEventListener('click', function () {
     ClassifyProfile('dislike');
 });
 
@@ -42,22 +67,22 @@ let index = 0;
 let currentProfile = '';
 let displayLDindex = 0;
 
-function nextProfile(){
+function nextProfile() {
     //De-stringify array from localStorage
     let array = JSON.parse(localStorage.getItem('fetched-profiles'));
     //store profile from array with current $index in $currentprofile
     currentProfile = array.results[index];
     displayProfile(currentProfile);
     //Update $index and re-execute fetchData() if>=9
-    if(index>=9){
-        index=0;
+    if (index >= 9) {
+        index = 0;
         fetchData();
-    }else{
+    } else {
         index++;
     }
 }
 
-function displayProfile(hooman){
+function displayProfile(hooman) {
     //Generating elements
     document.querySelector('.data-container').innerHTML = '';
     document.querySelector('.data-container').innerHTML += '<h1>' + hooman.name.first + ' ' + hooman.name.last + '</h1>';
@@ -66,13 +91,18 @@ function displayProfile(hooman){
     document.querySelector('.data-container').innerHTML += '<h3>Age: ' + hooman.dob.age + '</h3>';
     document.querySelector('.data-container').innerHTML += '<h3>Address: ' + hooman.location.street + ',</h3>';
     document.querySelector('.data-container').innerHTML += '<h3>' + hooman.location.city + '</h3>';
+    let targetCoords = hooman.location.coordinates;
+    console.log(typeof (localStorage.getItem('coords')));
+    let clientCoords = JSON.parse(localStorage.getItem('coords'));
+    console.log(clientCoords.latitude);
+    document.querySelector('.data-container').innerHTML += '<h3>' + getDistance(clientCoords.latitude, clientCoords.longitude, targetCoords.latitude, targetCoords.longitude) + '</h3>';
 }
 
-function ClassifyProfile(type){
-    if(type=='like'){
+function ClassifyProfile(type) {
+    if (type == 'like') {
         let likes = new Array();
         //De-stringifying liked profiles-array from localStorage except first like
-        if(localStorage.getItem('likes') != null){
+        if (localStorage.getItem('likes') != null) {
             likes = JSON.parse(localStorage.getItem('likes'));
         }
         likes.push(currentProfile);
@@ -80,10 +110,10 @@ function ClassifyProfile(type){
         localStorage.setItem('likes', JSON.stringify(likes));
         DisplayLikesDislikes();
         nextProfile();
-    }else if(type=='dislike'){
+    } else if (type == 'dislike') {
         let dislikes = new Array();
         //De-stringifying disliked profiles-array from localStorage except first dislike
-        if(localStorage.getItem('dislikes') != null){
+        if (localStorage.getItem('dislikes') != null) {
             dislikes = JSON.parse(localStorage.getItem('dislikes'));
         }
         dislikes.push(currentProfile);
@@ -94,18 +124,18 @@ function ClassifyProfile(type){
     }
 }
 
-function DisplayLikesDislikes(init){
+function DisplayLikesDislikes(init) {
     //Create $likes and set to de-stringified localstorage string likes
     let likes = JSON.parse(localStorage.getItem('likes'));
     //Loop through $likes and display
     document.querySelector('.likes-container').innerHTML = '';
-    if(likes != null){
-        for(i=0; i<likes.length;i++){
-        document.querySelector('.likes-container').innerHTML += '<h3 class="switchlist" id="l' + i + '">' + likes[i].name.first + ' ' + likes[i].name.last + '</h3>';
+    if (likes != null) {
+        for (i = 0; i < likes.length; i++) {
+            document.querySelector('.likes-container').innerHTML += '<h3 class="switchlist" id="l' + i + '">' + likes[i].name.first + ' ' + likes[i].name.last + '</h3>';
         }
-        for(a=0;a<likes.length;a++){
+        for (a = 0; a < likes.length; a++) {
             displayLDindex = a;
-            document.querySelector('#l' + a).addEventListener('click', function(){
+            document.querySelector('#l' + a).addEventListener('click', function () {
                 SwitchList(event.target.getAttribute('id').substr(1), 'likes');
             });
         }
@@ -114,29 +144,28 @@ function DisplayLikesDislikes(init){
     let dislikes = JSON.parse(localStorage.getItem('dislikes'));
     //Loop through $dislikes and display
     document.querySelector('.dislikes-container').innerHTML = '';
-    if(dislikes != null){
-        for(i=0; i<dislikes.length;i++){
-        document.querySelector('.dislikes-container').innerHTML += '<h3 class="switchlist" id="d' + i + '">' + dislikes[i].name.first + ' ' + dislikes[i].name.last + '</h3>';
+    if (dislikes != null) {
+        for (i = 0; i < dislikes.length; i++) {
+            document.querySelector('.dislikes-container').innerHTML += '<h3 class="switchlist" id="d' + i + '">' + dislikes[i].name.first + ' ' + dislikes[i].name.last + '</h3>';
         }
-        for(a=0;a<dislikes.length;a++){
+        for (a = 0; a < dislikes.length; a++) {
             displayLDindex = a;
-            document.querySelector('#d' + a).addEventListener('click', function(){
+            document.querySelector('#d' + a).addEventListener('click', function () {
                 SwitchList(event.target.getAttribute('id').substr(1), 'dislikes');
             });
         }
     }
-    addMarker();
 }
 
-function SwitchList(index, type){
+function SwitchList(index, type) {
     let likes = JSON.parse(localStorage.getItem('likes'));
     let dislikes = JSON.parse(localStorage.getItem('dislikes'));
     console.log(index);
     console.log(likes[index]);
-    if(type=='likes'){
+    if (type == 'likes') {
         dislikes.push(likes[index]);
         likes.splice(index, 1);
-    }else{
+    } else {
         likes.push(dislikes[index]);
         dislikes.splice(index, 1);
     }
@@ -148,16 +177,28 @@ function SwitchList(index, type){
 
 fetchData();
 
-//GeoLocation
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ2ZWxsb24iLCJhIjoiY2puNG1zdWd4M2kzdjNrcXlleGtpazI2cCJ9.p7i1IOVRm40GP4qdK4JuGg';
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v10'
-});
 
-function addMarker(){
-    let likes = JSON.parse(localStorage.getItem('likes'));
-for(i=0;i<likes.length;i++){
-    new mapboxgl.Marker().setLngLat([likes[i].location.coordinates.longitude, likes[i].location.coordinates.latitude]).addTo(map);
+function getDistance(clientLat, clientLon, targetLat, targetLon) {
+    console.log(clientLat);
+    console.log(clientLon);
+    console.log(targetLat);
+    console.log(targetLon);
+    let radlat1 = Math.PI * clientLat / 180;
+    let radlat2 = Math.PI * targetLat / 180;
+    let theta = clientLon - targetLon;
+    let radtheta = Math.PI * theta / 180;
+    let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+        dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return dist;
 }
+
+function locationSuccess(pos) {
+    console.log(pos);
+    localStorage.setItem('clientCoords', JSON.stringify(pos.coords));
 }
